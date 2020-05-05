dc62ab3e509f310453b6d823213998c1243fb814
Andy Teucher <andy.teucher@gov.bc.ca>
2020-05-05 16:32:21 -0700

    Change challenge in data structures, put challenge in template file

--- intro-r-May2020/04_data_structures.R
+++ intro-r-May2020/04_data_structures.R
@@ -23,7 +23,11 @@
 
 # Factors -----------------------------------------------------------------
 
-
+##### Challenge 1
+##
+## Look at the help for `?read_csv` to figure out how to control what data type
+## each column is read as. Then write a command so that `coat` is read as a
+## factor, and `likes_string` is read as a logical
 
 
 # Lists -------------------------------------------------------------------
