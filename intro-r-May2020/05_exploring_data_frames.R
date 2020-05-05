653395a3fb35c1dce30f5a0b9e838bad7296eb2b
Andy Teucher <andy.teucher@gov.bc.ca>
2020-05-05 16:43:03 -0700

    Ch 5 challenges and template

---
+++ intro-r-May2020/05_exploring_data_frames.R
@@ -0,0 +1,28 @@
+############### Exploring Data Frames ###############
+
+
+
+
+
+# Introducing the Gapminder data ------------------------------------------
+
+
+#### Challenge 1
+##
+## Read in the gapminder dataset using read_csv and assign it to a data frame
+## called `gapminder`
+##
+## How many rows and columns does `gapminder` have?
+##
+## What kind of data is in the `pop` and `continent` columns?
+
+
+
+
+
+
+#### Challenge 2
+##
+## It's good practice to also check the last few lines of your data.
+## How would you do this? How would check the last 15 lines?
+##
