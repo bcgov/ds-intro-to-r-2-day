68969d5541f8f548a546fc4611d769235a19a217
Sam Albers <sam.albers@gov.bc.ca>
2022-02-22 08:32:33 -0800

    update tidyr template script

--- intro-r-Feb2022/08_tidyr.R
+++ intro-r-Feb2022/08_tidyr.R
@@ -12,7 +12,7 @@
 # ## Challenge 1
 #
 # Is gapminder a purely long, purely wide, or some intermediate format?
-#
+
 
 
 
@@ -42,8 +42,8 @@
 
 # ## Challenge 2
 #
-# Using `gap_long`, calculate the mean life expectancy, population, and gdpPercap for each continent.
-#**Hint:** use the `group_by()` and `summarize()` functions we learned in the `dplyr` lesson
+#  Using `gap_long_split`, calculate the mean life expectancy, population, and gdpPercap for each continent.
+# **Hint:** use the `group_by()` and `summarize()` functions we learned in the `dplyr` lesson
 
 
 ## From long to intermediate format with pivot_wider()
@@ -73,4 +73,4 @@
 #
 # Take this 1 step further and create a `gap_ludicrously_wide` format data by pivoting over countries, year and the 3 metrics?
 #**Hint** this new dataframe should only have 5 rows.
-#
+
