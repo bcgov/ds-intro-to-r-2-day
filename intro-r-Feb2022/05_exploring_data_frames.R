114c3493d5a7985edb2cdaa742f5020f5ca69e62
Bonnie Robert <bonnie.robert@gov.bc.ca>
2022-02-22 13:41:48 -0800

    inserted challenges

--- intro-r-Feb2022/05_exploring_data_frames.R
+++ intro-r-Feb2022/05_exploring_data_frames.R
@@ -1,16 +1,9 @@
 ############### Exploring Data Frames ###############
 
 
-
-
-
 # Introducing the Gapminder data ------------------------------------------
 
-
 #### Challenge 1
-##
-## Read in the gapminder dataset using read_csv and assign it to a data frame
-## called `gapminder`
 ##
 ## How many rows and columns does `gapminder` have?
 ##
@@ -18,11 +11,20 @@
 
 
 
-
-
-
 #### Challenge 2
 ##
-## It's good practice to also check the last few lines of your data.
-## How would you do this? How would check the last 15 lines?
+## It's good practice to also check the last few lines of your data and some in the middle.
+## How would you do this?
+
+
+
+#### Challenge 3
 ##
+## Select all the rows in `gapminder` with data from 1975 and earlier
+
+
+
+#### Challenge 4
+
+## Select all the rows in `gapminder` with data from Canada, and just the columns year,
+## pop, and gdpPercap
