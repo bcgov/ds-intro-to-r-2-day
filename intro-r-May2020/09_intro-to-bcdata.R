194600ab57cfa981d5aafb66467d9a46a2ae89c1
Bonnie Robert <bonnie.robert@gov.bc.ca>
2023-01-03 10:42:22 -0800

    remove old course notes skeleton

---
+++ intro-r-May2020/09_intro-to-bcdata.R
@@ -0,0 +1,60 @@
+#################### Introduction to bcdata ################################
+
+
+## Getting started ---------------------------------------------------------
+
+
+
+
+## Searching the B.C. Data Catalogue ---------------------------------------
+
+
+
+
+## Importing data from the B.C. Data Catalogue -----------------------------
+
+
+
+
+## Challenge 1 --------------------------------------------------------------
+
+# Take a few minutes to search the B.C. Data Catalogue for data sets for a
+# different topic. Identify and import one tabular data set into your R session
+# (e.g. a CSV or XLS resource).
+
+
+
+
+
+## Practice manipulating data with dplyr  -----------------------------------
+
+
+
+
+## Challenge 2 ---------------------------------------------------------------
+
+# Using dplyr, determine the total number of books within each
+# Library System in British Columbia.
+
+
+
+
+## Practice plotting data with ggplot2 ---------------------------------------
+
+
+
+
+## Challenge 3 ---------------------------------------------------------------
+
+# Iterate the above plot to include information on the number of books
+# by Library Service. Add a title to the plot. Try other design changes
+# to make the plot more readable.
+
+
+
+
+## Using `bcdata` with spatial data in the B.C. Data Catalogue ----------------
+
+
+
+
---
+++ intro-r-May2020/README.md
@@ -0,0 +1,27 @@
+This folder contains the data (in the `data/` directory) as well as
+starter scripts that we will work through over the course of the
+workshop:
+
+Data:
+
+-   `feline-data_v2.csv`
+-   `feline-data.csv`
+-   `gapminder_asia_oceania.csv`
+-   `gapminder_data.csv`
+-   `gapminder_wide.csv`
+-   `gapminder-FiveYearData.csv`
+-   `inflammation.csv`
+-   `Owls.txt`
+
+R scripts:
+
+-   `04_data_structures.R`
+-   `05_exploring_data_frames.R`
+-   `06_ggplot2.R`
+-   `07_dplyr.R`
+-   `08_tidyr.R`
+-   `09_intro-to-bcdata.R`
+
+Rmarkdown documents:
+
+-   `11_rmarkdown-application.Rmd`
