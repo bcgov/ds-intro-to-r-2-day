aeda0c5a4775e103199fe002daa261497422d110
Bonnie Robert <bonnie.robert@gov.bc.ca>
2023-01-03 11:06:55 -0800

    add Jan course notes

---
+++ intro-r-Jan2023/05_exploring_data_frames.R
@@ -0,0 +1,31 @@
+############### Exploring Data Frames ###############
+
+
+# Introducing the Gapminder data ------------------------------------------
+
+
+#### Challenge 1
+##
+## How many rows and columns does `gapminder` have?
+##
+## What kind of data is in the `pop` and `continent` columns?
+
+
+
+#### Challenge 2
+##
+## It's good practice to also check the last few lines of your data and some in the middle.
+## How would you do this?
+
+
+
+#### Challenge 3
+##
+## Select all the rows in `gapminder` with data from 1975 and earlier
+
+
+
+#### Challenge 4
+
+## Select all the rows in `gapminder` with data from Canada, and just the columns year,
+## pop, and gdpPercap
