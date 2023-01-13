cf92161eadfcbe1912d2343b81811fe5334e231c
Bonnie Robert <bonnie.robert@gov.bc.ca>
2023-01-12 17:04:37 -0800

    add helloworld.Rmd

--- bin/helloworld.sh
+++ bin/helloworld.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
 cp index.Rmd intro-to-r-course-notes/
-#mv intro-to-r-course-notes/index.Rmd intro-to-r-course-notes/hello_world.Rmd
+mv intro-to-r-course-notes/index.Rmd intro-to-r-course-notes/hello_world.Rmd
 
 
