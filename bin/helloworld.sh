409e13a1d8bc7cd97fe4be76f63295505622c64b
Bonnie Robert <bonnie.robert@gov.bc.ca>
2023-01-12 17:52:55 -0800

    loop through .Rmds

--- bin/helloworld.sh
+++ bin/helloworld.sh
@@ -1,6 +1,17 @@
 #!bin/bash
 
-cp index.Rmd intro-to-r-course-notes/
-mv intro-to-r-course-notes/index.Rmd intro-to-r-course-notes/hello_world.Rmd
+reg="[0-9]{2}"
+rpl="---------------------------------------------------------------------"
 
+for x in *.Rmd
+do
+  if ! [[ $x =~ $reg ]]
+  then
+    continue
+  fi
 
+  x_out=${x/ds-intro-to-r-2-day\//}
+  x_out=${x_out/.Rmd/.R}
+  echo "creating ${x_out} from ${x}"
+
+done
