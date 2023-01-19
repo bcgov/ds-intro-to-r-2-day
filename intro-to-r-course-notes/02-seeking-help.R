175cd1cacfd6ca818fb5068e7fc600be77886255
runner <runner@Mac-1674157079105.local>
2023-01-19 19:44:32 +0000

    make course zipfile

---
+++ intro-to-r-course-notes/02-seeking-help.R
@@ -0,0 +1,79 @@
+#########  Seeking Help in R #########
+## Reading help files
+## --------------------------------------------------------------------- 
+
+
+
+
+## Special operators
+## --------------------------------------------------------------------- 
+
+
+
+
+## Getting help on packages
+## --------------------------------------------------------------------- 
+
+
+
+
+## Finding help for functions in packages
+## --------------------------------------------------------------------- 
+
+
+
+
+## When you kind of remember the function
+## --------------------------------------------------------------------- 
+
+
+
+
+## When you have no idea where to begin
+## --------------------------------------------------------------------- 
+
+
+
+
+## Challenges (10 minutes)
+## --------------------------------------------------------------------- 
+
+
+
+
+#### Challenge 1
+## --------------------------------------------------------------------- 
+
+
+
+
+# Look at the help for the `sum()` function. What are two ways you can pass numbers 
+# into the function so they are added together?
+#### Challenge 2
+## --------------------------------------------------------------------- 
+
+
+
+
+# Look at the help for the `paste()` function. You'll need to use this later.
+# What is the difference between the `sep` and `collapse` arguments?
+#### Challenge 3
+## --------------------------------------------------------------------- 
+
+
+
+
+# Use help to find a function (and its associated parameters) that you could
+# use to load data from a csv file in which columns are delimited with "\t"
+# (tab) and the decimal point is a "." (period). This check for decimal
+# separator is important, especially if you are working with international
+# colleagues, because different countries have different conventions for the
+# decimal point (i.e. comma vs period).
+# hint: use `??csv` to lookup csv related functions.
+#### Challenge 4
+## --------------------------------------------------------------------- 
+
+
+
+
+# Find the help for the `mutate` function in the `dplyr` package. What is its purpose? What do you notice different about this help page?
