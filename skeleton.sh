4c05898bdfc81739db313640d0d064654d19974f
Bonnie Robert <bonnie.robert@gov.bc.ca>
2023-01-19 10:36:45 -0800

    minor edits

--- skeleton.sh
+++ skeleton.sh
@@ -4,14 +4,14 @@ echo "running the script file skeleton.sh"
 reg="[0-9]{2}"
 rpl="---------------------------------------------------------------------"
 
-for x in ds-intro-to-r-2-day/*.Rmd
+for x in *.Rmd
 do
   if ! [[ $x =~ $reg ]]
   then
     continue
   fi
 
-  x_out=${x/ds-intro-to-r-2-day\//}
+  #x_out=${x/ds-intro-to-r-2-day\//}
   x_out=${x_out/.Rmd/.R}
   echo "creating ${x_out} from ${x}"
 
